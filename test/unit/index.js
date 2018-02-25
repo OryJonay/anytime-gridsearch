cd5271538ca40a2f897e5fec62f15c97c578714e
ory <ory@cronus-cyber.com>
2018-02-25 16:09:23 +0200

    more unit tests for GridToolbar - mocking the /datasets/ get method

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
 Vue.config.productionTip = false
+Vue.config.silent = true
 
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context('./specs', true, /\.spec$/)
--- test/unit/specs/GridsToolbar.spec.js
+++ test/unit/specs/GridsToolbar.spec.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
+import moxios from 'moxios'
 import GridsToolbar from '@/components/GridsToolbar'
 
 describe('GridsToolbar.vue', () => {
+  beforeEach(function () {
+    moxios.install()
+  })
+  afterEach(function () {
+    moxios.uninstall()
+  })
   it('Should render correct toolbar title', () => {
     const Constructor = Vue.extend(GridsToolbar)
     const vm = new Constructor().$mount()
-    expect(vm.selected_dataset).to.equal('')
     expect(vm.$el.querySelector('.hidden-sm-and-down').textContent)
       .to.equal('AnyTimeGridSearch github')
   })
@@ -13,5 +19,16 @@ describe('GridsToolbar.vue', () => {
     const Constructor = Vue.extend(GridsToolbar)
     const vm = new Constructor().$mount()
     expect(vm.selected_dataset).to.equal('')
+  })
+  it('Should mount and get a list of datasets', () => {
+    const Constructor = Vue.extend(GridsToolbar)
+    const vm = new Constructor().$mount()
+    moxios.stubRequest('/datasets/', {
+      status: 200,
+      response: [{'name': 'IRIS', 'examples': 'http://127.0.0.1:8000/datasets/datasets/IRIS/examples.csv', 'labels': 'http://127.0.0.1:8000/datasets/datasets/IRIS/labels.csv'}]
+    })
+    moxios.wait(function () {
+      expect(vm.data().datasets.length).to.equal(1)
+    })
   })
 })
