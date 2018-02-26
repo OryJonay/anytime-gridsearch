7661c3b04417e98830ae3ab4017ad9d1d3262662
ory <ory@cronus-cyber.com>
2018-02-26 17:59:59 +0200

    more tests for GridToolbar component

--- test/unit/specs/GridsToolbar.spec.js
+++ test/unit/specs/GridsToolbar.spec.js
@@ -9,16 +9,23 @@ describe('GridsToolbar.vue', () => {
   afterEach(function () {
     moxios.uninstall()
   })
+  it('Should have 2 toolbar items items', () => {
+    const Constructor = Vue.extend(GridsToolbar)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelectorAll('v-toolbar-items').length)
+      .to.equal(2)
+  })
+  it('Should have 4 toolbar item items', () => {
+    const Constructor = Vue.extend(GridsToolbar)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelectorAll('v-toolbar-item').length)
+      .to.equal(4)
+  })
   it('Should render correct toolbar title', () => {
     const Constructor = Vue.extend(GridsToolbar)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hidden-sm-and-down').textContent)
       .to.equal('AnyTimeGridSearch github')
-  })
-  it('Should mount with blank dataset', () => {
-    const Constructor = Vue.extend(GridsToolbar)
-    const vm = new Constructor().$mount()
-    expect(vm.selected_dataset).to.equal('')
   })
   it('Should mount and get a list of datasets', () => {
     const Constructor = Vue.extend(GridsToolbar)
@@ -30,5 +37,20 @@ describe('GridsToolbar.vue', () => {
     moxios.wait(function () {
       expect(vm.data().datasets.length).to.equal(1)
     })
+  })
+  it('Should mount with blank dataset', () => {
+    const Constructor = Vue.extend(GridsToolbar)
+    const vm = new Constructor().$mount()
+    expect(vm.selected_dataset).to.equal('')
+  })
+  it('Should mount with blank classifiers', () => {
+    const Constructor = Vue.extend(GridsToolbar)
+    const vm = new Constructor().$mount()
+    expect(vm.classifiers.length).to.equal(0)
+  })
+  it('Should mount with blank dataset names', () => {
+    const Constructor = Vue.extend(GridsToolbar)
+    const vm = new Constructor().$mount()
+    expect(vm.dataset_names.length).to.equal(0)
   })
 })
